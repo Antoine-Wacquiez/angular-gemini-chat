d4a01a1445ce40cf37a34d91388b9c602324d519
Antoine Wacquiez <wacquiez.antoine@gmail.com>
2026-01-07 22:10:45 +0100

    add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  geminiApiKey: 'AIzaSyDm5DV1KOVszpEucXwjmPmtWxlJVypyyHg',
+  geminiApiKey: 'AIzaSyDPpAAbd2ic9AkMx8V1wXCiD8V38Wbw3-E',
   geminiApiUrl: 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent'
 };
